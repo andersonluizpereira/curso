95f4f34acfcac2222b05cfbd64351b9f7a6acf66
anderson luiz pereira <andy2903.alp@gmail.com>
2015-07-17 10:05:19 -0300

    Criando Config. EMAIL

---
+++ Quiron.LojaVirtual.Dominio/Entidades/EmailConfiguracoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+  public class EmailConfiguracoes
+    {
+      public bool UsarSsl = false;
+
+      public string ServidorSmtp = "smtp.servidor.com.br";
+
+      public int ServidorPorta = 587;
+
+      public string Usuario = "usuarioEmail";
+
+      public bool EscreverArquivo = false;
+
+      public string PastaArquivo = @"C:\envioemail";
+
+      public string De = "artemmis@artemmis.com.br";
+
+      public string Para = "artemmis@artemmis.com.br";
+    }
+}
