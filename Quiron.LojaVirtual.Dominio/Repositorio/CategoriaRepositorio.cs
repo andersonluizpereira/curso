b2193f9f8d2260425d4484915a34974b0bbf9f4c
anderson luiz pereira <andy2903.alp@gmail.com>
2015-08-27 20:50:32 -0300

    Aula56

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/CategoriaRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+   public class CategoriaRepositorio
+    {
+       private readonly EfDbContext _context = new EfDbContext();
+
+       public IEnumerable<Categoria> ObteCategorias()
+       {
+
+           return _context.Categorias.OrderBy(c => c.CategoriaDescricao);
+
+       }
+
+    }
+}
